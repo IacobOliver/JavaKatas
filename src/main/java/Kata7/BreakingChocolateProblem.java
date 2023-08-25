f3ea7fdb5dd6fe0fd6fb6fdcd15a9d1b2c985c31
Oliver <olialexander08@gmail.com>
2023-08-25 15:08:37 +0300

    kata7 done

---
+++ src/main/java/Kata7/BreakingChocolateProblem.java
@@ -0,0 +1,12 @@
+package Kata7;
+
+public class BreakingChocolateProblem {
+
+    public static void main(String[] args) {
+       breakChocolate(2,3);
+    }
+
+    public static long breakChocolate(long n, long m) {
+        return n*m-1;
+    }
+}
